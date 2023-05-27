ac8b17757ff5282a4c4af242402fbc65e2f3ddaa
jiwon615 <seg615@naver.com>
2023-05-27 15:24:58 +0900

    feat: LogFilter 추가 및 API Gateway를 UserService 와ㅏ 연동

--- src/main/java/com/jimart/apigateway/core/filter/LogFilter.java
+++ src/main/java/com/jimart/apigateway/core/filter/LogFilter.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -12,30 +14,30 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LogFilter extends AbstractGatewayFilterFactory<LogFilter.Config> {
 
-    public GlobalFilter() {
+    public LogFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
+        return new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info(">> Global Filter baseMessage: {}", config.getBaseMessage());
+            log.info(">> Logging Filter baseMessage: {}", config.getBaseMessage());
 
             if (config.isPreLogger()) {
-                log.info(">> Global Filter Start: request id -> {}", request.getId());
+                log.info(">> Logging PRE Filter: request id -> {}", request.getId());
             }
 
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
-                    log.info(">> Global Filter End: response code -> {}", response.getStatusCode());
+                    log.info(">> Logging POST Filter: response code -> {}", response.getStatusCode());
                 }
             }));
-        };
+        }, Ordered.LOWEST_PRECEDENCE);
     }
 
     @Getter
